d8237e3bf23d689120632eafee04fce2a29672a3
sannalamani <s.manik@outlook.fr>
2024-11-04 17:05:25 +0530

    Updated portfolio with latest changes

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,30 @@ const About = () => (
     <div className="container mx-auto px-6">
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-12">
+  
+        {/* Experience Section */}
+        <div className="bg-blue-200 p-6 rounded-lg shadow-md">
+          <h3 className="text-2xl font-bold mb-4">Work Experience</h3>
+          
+          {/* Sesamers Experience */}
+          <p className="font-semibold">Full Stack Developer</p>
+          <p className="mb-2">Sesamers, Paris (03/2024 - 09/2024)</p>
+          <p className="text-sm mb-4">
+            Developed a SaaS platform from scratch to connect startups and venture capitalists (VCs), and to serve as a job board. 
+            <br />Technology stack: NextJS, MongoDB, Tailwind CSS, WordPress, and PHP.
+            <br />Worked on design, planning, development, and deployment, and created innovative solutions to meet business goals.
+          </p>
+          
+          {/* Infosys Experience */}
+          <p className="font-semibold">Systems Engineer</p>
+          <p className="mb-2">Infosys, Bangalore (11/2020 - 09/2021)</p>
+          <p className="text-sm">
+            Assisted in developing and maintaining front-end web applications using React.js and Redux to create dynamic and responsive user interfaces
+            <br />Debugged and resolved UI bugs and issues by inspecting browser developer tools, ensuring optimal functionality and user experience across different browsers and devices
+            <br />Certified as an Infosys Software Programmer.
+          </p>
+        </div>
+
         <div className="bg-blue-200 p-6 rounded-lg shadow-md">
           <h3 className="text-2xl font-bold mb-4">Education</h3>
           <p className="font-semibold">MSc - Software Engineering</p>
@@ -22,28 +46,6 @@ const About = () => (
           <ul className="list-disc list-inside">
             <li>Android Application for Pharmacy</li>
           </ul>
-        </div>
-  
-        {/* Experience Section */}
-        <div className="bg-blue-200 p-6 rounded-lg shadow-md">
-          <h3 className="text-2xl font-bold mb-4">Work Experience</h3>
-          
-          {/* Sesamers Experience */}
-          <p className="font-semibold">Software Developer</p>
-          <p className="mb-2">Sesamers, Paris (03/2024 - 09/2024)</p>
-          <p className="text-sm mb-4">
-            Developed a SaaS platform from scratch to connect startups and venture capitalists (VCs), and to serve as a job board. 
-            <br />Technology stack: NextJS, MongoDB, Tailwind CSS, WordPress, and PHP.
-            <br />Worked on design, planning, development, and deployment, and created innovative solutions to meet business goals.
-          </p>
-          
-          {/* Infosys Experience */}
-          <p className="font-semibold">Systems Engineer</p>
-          <p className="mb-2">Infosys, Bangalore (11/2020 - 09/2021)</p>
-          <p className="text-sm">
-            SAP BASIS, SAP SD. 
-            <br />Certified as an Infosys Software Programmer.
-          </p>
         </div>
       </div>
   
